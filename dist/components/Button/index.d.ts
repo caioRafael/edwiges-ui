cf4e17f6a620bce257f0a356fb726083d5449353
caioRafael <caiorafaelrg@gmail.com>
2022-06-18 12:34:13 -0300

    remove dist

--- dist/components/Button/index.d.ts
+++
@@ -1,7 +0,0 @@
-import React, { ReactNode } from "react";
-interface Props {
-    children: ReactNode;
-}
-export declare const Button: React.FC<Props>;
-export {};
-//# sourceMappingURL=index.d.ts.map
--- dist/components/Button/index.js
+++
@@ -1,5 +0,0 @@
-import React from "react";
-export const Button = ({ children }) => {
-    return (React.createElement("button", null, children));
-};
-//# sourceMappingURL=index.js.map
