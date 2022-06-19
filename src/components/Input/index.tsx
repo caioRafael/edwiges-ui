fdb74a52c764f44b972e93672b45d0a1b259f476
caioRafael <caiorafaelrg@gmail.com>
2022-06-18 22:50:48 -0300

    input

--- src/components/Input/index.tsx
+++ src/components/Input/index.tsx
@@ -1,7 +1,40 @@
-import React from "react";
+import React, { Component, ReactNode } from "react";
+import { Label } from "./styles";
 
-export const Input: React.FC = () => {
+export interface Props {
+    name: string;
+    value: string;
+    setValue: (text?: string) => void;
+    type?: string;
+    color?: string;
+    placeholder?: string;
+    icon?: ReactNode;
+  }
+  
+export interface ILabel {
+    icon?: ReactNode;
+  }
+  
+export const Input: React.FC<Props> = ({
+    name,
+    type = 'text',
+    color = '#1B2CC1',
+    value,
+    setValue,
+    placeholder,
+    icon,
+  }) => {
     return(
-        <></>
+        <Label htmlFor={name} color={color} icon={icon}>
+            <input
+                name={name}
+                type={type}
+                aria-label={name}
+                value={value}
+                placeholder={placeholder}
+                onChange={(e) => setValue(e.target.value)}
+            />
+            {icon && icon}
+        </Label>
     )
 }
--- src/components/Input/styles.ts
+++ src/components/Input/styles.ts
@@ -1,5 +1,44 @@
 import styled from "styled-components";
+import { ILabel } from ".";
 
-export const Container = styled.input`
-    height: 40px;
+export const Label = styled.label<ILabel>`
+    display: flex;
+    position: relative;
+
+    > input {
+        background: #ABD2FA;
+        border: 2px solid #91B2FD;
+        border-radius: 5px;
+        display: block;
+        outline: none;
+
+        margin: 5px;
+        padding: 12px;
+        padding-left: ${(props) => (props.icon ? '40px' : '12px')};
+
+        color: #3D518C;
+        font-size: 16px;
+
+        transition: 180ms ease-in-out;
+
+        &::placeholder{
+            color: #3D518C;
+        }
+
+        ~ svg {
+            fill: #1B2CC1;
+            position: absolute;
+            left: 12px;
+            top: 16px;
+            width: 24px;
+            height: 24px;
+            transition: 180ms ease-in-out;
+        }
+        &:focus {
+            border: 2px solid ${(props) => props.color};
+            ~ svg {
+                fill: ${(props) => props.color};
+            }
+        }
+    }
 `
